dceb55634ad9331726daa1e343c578e94610d74d
Jaeha Yi <58238887+jaehayi25@users.noreply.github.com>
2020-07-22 16:03:09 -0600

    Add files via upload

--- interface.js
+++ interface.js
@@ -37,7 +37,7 @@ function getResponse(percents) {
 	}
 	response_arr = myArr.intents[maxPos].responses;
 	response = response_arr[Math.floor(Math.random() * response_arr.length)];
-	if (response == "Sure, how?" || response == "Ok. What should I bring?" || response == "How will I play?" || response == "What should I bring?" || response == "How will I get there?" || response == "What should we do there?" || reponse == "Alright! Where we partying?") {
+	if (response == "Sure, how?" || response == "Ok. What should I bring?" || response == "How will I play?" || response == "What should I bring?" || response == "How will I get there?" || response == "What should we do there?" || response == "Alright! Where we partying?") {
 		nextResponse = affirmative[Math.floor(Math.random() * affirmative.length)];
 	} 
 	return response;
